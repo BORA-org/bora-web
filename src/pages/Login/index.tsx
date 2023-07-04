75ca7a4f6dc8e0e4945fd25c879c908103e3815b
Neylane <neylanepl@gmail.com>
2023-07-04 14:50:11 -0300

    feat: template do login

--- src/pages/Login/index.tsx
+++ src/pages/Login/index.tsx
@@ -1,14 +1,122 @@
-import React from 'react';
+import React, { useState } from 'react';
 
-const Login = () => {
+interface LoginState {
+    cpfCnpj: string;
+    password: string;
+    rememberPassword: boolean;
+}
+  
+const Login: React.FC = () => {
+    const [loginState, setLoginState] = useState<LoginState>({
+      cpfCnpj: '',
+      password: '',
+      rememberPassword: false,
+    });
+  
+    const handleLogin = (e: React.FormEvent) => {
+      e.preventDefault();
+      // Lógica para realizar a autenticação do usuário
+    };
+  
+    const handleInputChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+      const { name, value, type, checked } = e.target;
+      const inputValue = type === 'checkbox' ? checked : value;
+  
+      setLoginState((prevState) => ({
+        ...prevState,
+        [name]: inputValue,
+      }));
+    };
+  
+    const handleForgotPassword = () => {
+      // Lógica para lidar com a recuperação de senha
+    };
+  
     return (
-        <div>
-            <h1 className="font-gilroy-bold leading-normal text-5xl text-gray-g5">Acesso ao sistema</h1>
-            <h1 className="font-gilroy-regular leading-normal text-2xl text-gray-g5">CPF/CNPJ:</h1>
-            <h1 className="font-gilroy-light leading-normal text-base text-black-b2">Lembrar de mim</h1>
-            <h1 className="font-gilroy-medium leading-normal text-4xl text-gray-g5">Entrar</h1>
-        </div>
-    );
-};
+        <>
+          <div className="flex min-h-full flex-1 flex-col justify-center px-6 py-12 lg:px-8">
+            <div className="sm:mx-auto sm:w-full sm:max-w-sm">
+              <h2 className="mt-10 text-center text-2xl font-bold leading-9 tracking-tight text-gray-900">
+                Acesso ao sistema
+              </h2>
+            </div>
+    
+            <div className="mt-10 sm:mx-auto sm:w-full sm:max-w-sm">
+              <form className="space-y-6" action="#" method="POST">
+                <div>
+                  <label htmlFor="cpf/cnpj" className="block text-sm font-medium leading-6 text-gray-900">
+                    CPF/CNPJ:
+                  </label>
+                  <div className="mt-2">
+                    <input
+                      id="cpf/cnpj"
+                      name="cpf/cnpj"
+                      type="text"
+                      autoComplete="cpf/cnpj"
+                      required
+                      className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+                    />
+                  </div>
+                </div>
+    
+                <div>
+                  <div className="flex items-center justify-between">
+                    <label htmlFor="password" className="block text-sm font-medium leading-6 text-gray-900">
+                      Senha:
+                    </label>
+                  </div>
+                  <div className="mt-2">
+                    <input
+                      id="password"
+                      name="password"
+                      type="password"
+                      autoComplete="current-password"
+                      required
+                      className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+                    />
+                  </div>
+                  <div className="flex items-center justify-between">
+                  <label className="flex items-center">
+                    <input
+                        type="checkbox"
+                        name="rememberPassword"
+                        checked={loginState.rememberPassword}
+                        onChange={handleInputChange}
+                        className="mr-2"
+                        />
+                        <span className="text-sm">Lembrar de mim</span>
+                    </label>
+                    <div className="text-sm">
+                      <a href="#" className="font-semibold hover:text-indigo-500">
+                        Esqueceu a senha?
+                      </a>
+                    </div>
+                  </div>
+                </div>
+    
+                <div>
+                  <button
+                    type="submit"
+                    className="flex w-full justify-center rounded-md bg-indigo-600 px-3 py-1.5 text-sm font-semibold leading-6 text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
+                  >
+                    Entrar
+                  </button>
+                </div>
+
+                <div>
+                  <button
+                    type="submit"
+                    className="flex w-full justify-center rounded-md bg-indigo-600 px-3 py-1.5 text-sm font-semibold leading-6 text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
+                  >
+                    Não tenho conta
+                  </button>
+                </div>
+              </form>
+    
+            </div>
+          </div>
+        </>
+      )
+  };
 
 export default Login;
