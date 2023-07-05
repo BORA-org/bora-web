4cd167f92558927605fe124bed5f5562592ccaf6
Neylane <neylanepl@gmail.com>
2023-07-05 07:58:27 -0300

    feat: parte da integracão com o back

--- src/pages/Login/index.tsx
+++ src/pages/Login/index.tsx
@@ -1,9 +1,14 @@
 import React, { useState } from 'react';
+import axios, {AxiosResponse, AxiosError } from 'axios';
 
 interface LoginState {
     cpfCnpj: string;
     password: string;
     rememberPassword: boolean;
+}
+
+interface ApiResponse {
+    token: string;
 }
   
 const Login: React.FC = () => {
@@ -14,8 +19,23 @@ const Login: React.FC = () => {
     });
   
     const handleLogin = (e: React.FormEvent) => {
-      e.preventDefault();
-      // Lógica para realizar a autenticação do usuário
+        e.preventDefault();
+
+        const payload = {
+            email: loginState.cpfCnpj,
+            password: loginState.password
+        }
+
+        axios.post<ApiResponse>('/api/autenticacao', payload)
+        .then((response: AxiosResponse<ApiResponse>) => {
+            // Autenticação bem-sucedida, você pode obter o token de autenticação ou outras informações relevantes da resposta
+            const token = response.data.token;
+            console.log('Autenticação bem-sucedida!');
+        })
+        .catch((error: AxiosError) => {
+            // Autenticação falhou, exiba uma mensagem de erro ou tome outras medidas apropriadas
+            console.log('Autenticação falhou!', error);
+        });
     };
   
     const handleInputChange = (e: React.ChangeEvent<HTMLInputElement>) => {
@@ -42,18 +62,20 @@ const Login: React.FC = () => {
             </div>
     
             <div className="mt-10 sm:mx-auto sm:w-full sm:max-w-sm">
-              <form className="space-y-6" action="#" method="POST">
+                <form className="space-y-6" onSubmit={handleLogin}>
                 <div>
-                  <label htmlFor="cpf/cnpj" className="block text-sm font-medium leading-6 text-gray-900">
+                  <label htmlFor="cpfCnpj" className="block text-sm font-medium leading-6 text-gray-900">
                     CPF/CNPJ:
                   </label>
                   <div className="mt-2">
                     <input
-                      id="cpf/cnpj"
-                      name="cpf/cnpj"
+                      id="cpfCnpj"
+                      name="cpfCnpj"
                       type="text"
-                      autoComplete="cpf/cnpj"
+                      autoComplete="cpfCnpj"
                       required
+                      value={loginState.cpfCnpj}
+                      onChange={handleInputChange}
                       className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
                     />
                   </div>
@@ -72,11 +94,13 @@ const Login: React.FC = () => {
                       type="password"
                       autoComplete="current-password"
                       required
+                      value={loginState.password}
+                      onChange={handleInputChange}
                       className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
                     />
                   </div>
                   <div className="flex items-center justify-between">
-                  <label className="flex items-center">
+                    <label className="flex items-center">
                     <input
                         type="checkbox"
                         name="rememberPassword"
@@ -96,7 +120,7 @@ const Login: React.FC = () => {
     
                 <div>
                   <button
-                    type="submit"
+                    type="submit" value="submit"
                     className="flex w-full justify-center rounded-md bg-indigo-600 px-3 py-1.5 text-sm font-semibold leading-6 text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
                   >
                     Entrar
