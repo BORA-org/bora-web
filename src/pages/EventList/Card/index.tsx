b7665190c8ac6eafe8caa86594ceb5db6fffe94a
Mariane Felix <marianefelix59@gmail.com>
2023-07-06 16:58:48 -0300

    feat: finaliza estilização da listagem de eventos

--- src/pages/EventList/Card/index.tsx
+++ src/pages/EventList/Card/index.tsx
@@ -1,45 +1,55 @@
-import React, { useState } from "react"
+import React from "react"
 import EventImage from '../../../assets/img/event.jpg';
-const images = [
-    'event-1.jpg',
-    'event-2.jpg',
-    'event-3.jpg',
-    'event-4.jpg',
-    'event.jpg',
-];
+import { ReactComponent as CalendarIcon } from '../../../assets/img/calendar.svg';
+import { ReactComponent as LocatizationIcon } from '../../../assets/img/map.svg';
+import { ReactComponent as WalletIcon } from '../../../assets/img/wallet.svg';
+import { ReactComponent as EditIcon } from '../../../assets/img/edit.svg';
+import { ReactComponent as RemoveIcon } from '../../../assets/img/trash.svg';
+
+import './styles.css';
 
 export const Card = () => {
-    const [activeIndex, setActiveIndex] = useState(0);
-
-    const handleNextSlide = () => {
-        setActiveIndex((prevIndex) => (prevIndex + 1) % images.length);
-    };
-
-    const handlePrevSlide = () => {
-        setActiveIndex((prevIndex) => (prevIndex - 1 + images.length) % images.length);
-    };
-    
     return (
-        <div className="w-100 bg-white-w2 rounded-3xl shadow-md">
+        <div className="bg-white-w2 rounded-3xl shadow-md">
             <div className="flex">
-                <div className="w-100 relative">
-                <img src={EventImage} alt="" className="w-200 h-150" />
-                <button
-                    className="w-7 h-7 bg-white-w2 absolute top-1/2 left-2 transform -translate-y-1/2 rounded-full"
-                    onClick={handlePrevSlide}
-                >
-                    &lt;
-                </button>
-                <button
-                    className="w-7 h-7 bg-white-w2 absolute top-1/2 right-2 transform -translate-y-1/2 rounded-full"
-                    onClick={handleNextSlide}
-                >
-                    &gt;
-                </button>
+                <div className="relative w-2/5">
+                    <img src={EventImage} alt="" className="rounded-l-3xl w-full h-full" />
                 </div>
-                <div className="w-1/2 p-4">
+                <div className="p-4 pr-6">
                     <h2 className="font-gilroy-bold text-3xl text-black-b1 text-opacity-70">Título do Card</h2>
-                    <p className="font-gilroy text-gray-g5">Música popular brasileira ao vivo, ambiente adaptado e segurança nas entradas.</p>
+                    <p className="font-gilroy-medium text-gray-g5 text-sm leading-4">Música popular brasileira ao vivo, ambiente adaptado e segurança nas entradas.</p>
+                    <div className="flex justify-between">
+                        <div className="flex flex-col gap-1 mt-4">
+                            <p className="flex items-center gap-1 font-gilroy-medium text-gray-g5 text-sm leading-4">
+                                <CalendarIcon />
+                                01/01/2022 - 20:00
+                            </p>
+                            <p className="flex items-center gap-1 font-gilroy-medium text-gray-g5 text-sm leading-4">
+                                <LocatizationIcon />
+                                Rua aleatória, 2020 - Natal/RN
+                            </p>
+                            <p className="flex items-center gap-1 font-gilroy-medium text-gray-g5 text-sm leading-4">
+                                <WalletIcon />
+                                R$50,00
+                            </p>
+                        </div>
+                        <div className="flex flex-col">
+                            <div className="flex items-center mb-3">
+                            <label className="switch">
+                                <input type="checkbox" />
+                                <span className="slider round"></span>
+                            </label>
+                            </div>
+                            <div className="flex justify-between">
+                                <button className="flex items-center gap-2 text-sm font-gilroy-regular" onClick={() => {}}>
+                                    <EditIcon className="w-4 h-4" />
+                                </button>
+                                <button className="flex items-center gap-2 text-sm font-gilroy-regular"  onClick={() => {}}>
+                                    <RemoveIcon className="w-4 h-4" />
+                                </button>
+                            </div>
+                        </div>
+                    </div>
                 </div>
             </div>
         </div>
--- src/pages/EventList/index.tsx
+++ src/pages/EventList/index.tsx
@@ -1,44 +1,32 @@
 import React from 'react';
-import { ReactComponent as Logout } from '../../assets/img/sign_out.svg';
 import Button from '../../ds/components/Button';
 import { useNavigate } from 'react-router-dom';
-import Logo from '../../assets/img/logo_white.png';
-
-import './styles.css';
 import { Card } from './Card';
+import { Header } from '../../ds/components/Header';
+import './styles.css';
 
 export const EventList = () => {
     const navigate = useNavigate();
 
     return (
-        <div className="bg-gray-g1">
-            <header className="bg-blue-b1 text-white-w1 pt-4 pb-4 pl-40 flex justify-between items-center">
-                <div className="flex items-center">
-                    <img src={Logo} alt="Logo" className="w-12" />
-                    <span className="ml-2 text-3xl font-gilroy-bold">BORA? PRODUTOR</span>
+        <div className="bg-gray-g1 flex flex-col min-h-screen">
+            <Header />
+            <main className='flex-grow w-2/5 mx-auto flex flex-col mt-12 pb-10'>
+                <Button width='w-100' text="+ Criar evento" onClick={() => navigate('/add-event')} />
+                <h1 className='font-gilroy-bold text-3xl text-black-b1 text-opacity-70 mt-12 mb-2'>
+                    Eventos publicados
+                </h1>
+                <div className="flex flex-col gap-10">
+                    <Card />
+                    <Card />
+                    <Card />
                 </div>
-                <div className="flex items-center gap-14">
-                    <div className="flex items-center">
-                        <div className="circle-image mr-2 bg-white-w1" />
-                        <div>
-                            <p className="font-gilroy-medium">John Doe</p>
-                            <p className="text-sm font-gilroy-light">john.doe@example.com</p>
-                        </div>
-                    </div>
-                    <button className="flex items-center gap-2 pr-20 text-base font-gilroy-regular" onClick={() => {}}>
-                        <Logout className="w-6 h-6" />
-                        Sair
-                    </button>
+                <h1 className='font-gilroy-bold text-3xl text-black-b1 text-opacity-70 mt-12 mb-2'>
+                    Eventos pendentes
+                </h1>
+                <div className="flex flex-col gap-10">
+                    <Card />
                 </div>
-            </header>
-            <main className='ml-30 mr-30 flex flex-col items-center'>
-                <Button text="Criar evento" onClick={() => navigate('/add-event')}/>
-                <div>
-                    <h1 className='font-gilroy-bold text-3xl text-black-b1 text-opacity-70'>
-                        Eventos publicados
-                    </h1>
-                </div>
-                <Card />
             </main>
         </div>
     );
