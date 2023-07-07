d9bed9ae4a4729c50369a2ff3da6d8fc8c41dd9c
marianefelix <mariane.felix.dev@gmail.com>
2023-07-07 20:16:42 -0300

    feat: integra logout

--- src/services/ProtectRoute/index.tsx
+++ src/services/ProtectRoute/index.tsx
@@ -1,15 +1,18 @@
 import { ReactElement } from 'react'
 import { Navigate } from 'react-router-dom'
+import { getToken } from '../Api/auth';
 
 interface ProtectRouteProps {
     children: ReactElement;
 }
 
 const ProtectRoute = ({ children }: ProtectRouteProps) => {
-    const isAuthenticated: boolean = true;
+    const isAuthenticated: boolean = getToken() !== null;
+
     if (isAuthenticated) {
         return children;
     }
+
     return <Navigate to="/" replace />;
 }
 
