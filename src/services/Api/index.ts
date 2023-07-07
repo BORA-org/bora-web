ded567b1573324069a02ae39313c2efc722d9b66
Neylane <neylanepl@gmail.com>
2023-07-07 13:15:02 -0300

    fix: getToken Authorization

--- src/services/Api/index.ts
+++ src/services/Api/index.ts
@@ -1,10 +1,11 @@
 import axios, { AxiosInstance } from 'axios';
+import { getToken } from './auth';
 
 const API: AxiosInstance = axios.create({
     baseURL: 'http://3.84.165.44:8080/api',
     headers: {
         'Content-Type': 'application/json',
-        Authorization: 'Bearer your_token_here' // Insira o token de autorização adequado
+        Authorization: `Bearer ${getToken()}`
     }
 });
 
