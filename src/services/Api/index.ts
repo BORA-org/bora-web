a4782e657131e9a23d8859068f9d95b00a1b7606
Neylane <neylanepl@gmail.com>
2023-07-07 12:53:06 -0300

    fix: IP

--- src/services/Api/index.ts
+++ src/services/Api/index.ts
@@ -1,7 +1,7 @@
 import axios, { AxiosInstance } from 'axios';
 
 const API: AxiosInstance = axios.create({
-    baseURL: 'http://3.139.60.209/api',
+    baseURL: 'http://3.84.165.44:8080/api',
     headers: {
         'Content-Type': 'application/json',
         Authorization: 'Bearer your_token_here' // Insira o token de autorização adequado
